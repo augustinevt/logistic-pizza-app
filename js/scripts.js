6a4783ddf024b507bb868a18ded6be90fc6eae1e
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-06-17 10:57:03 -0700

    make pizzaQue use user input to instantiate a new pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,56 @@
-//
-// function getAddress() {
-//   var address = prompt('please fill in address');
-// }
-//
-// function pizzaQue() {
-//   alert("this is the start of PizzaQue");
-//   while(true){
-//     var ingredient = prompt('what to you want on it?')
-//     //push to contructor aurgument object
-//     if(ingredient === ''){break;}
-//   }
-//   alert("push Pizza!");
-//   var addPizza = prompt('add a pizza?');
-//   if(addPizza === "yes") {
-//     pizzaQue();
-//   }else{
-//     return;
-//   }
-// }
-//
-// function orderQue() {
-//   alert("this is the start of orderQue");
-//   pizzaQue();
-//   var addOrder = prompt('add an order?');
-//   if (addOrder === 'yes') {
-//     orderQue();
-//   }else{
-//     return;
-//   }
-// }
-//
-//
-// $(function() {
-//   prompt("hello! What's your name?");
-//   orderQue();
-//
-// });
+function Pizza(obj) {
+  for(var key in obj) {
+    this[key] = obj[key];
+  }
+}
+
+
+////////
+
+function getAddress() {
+  var address = prompt('please fill in address');
+}
+
+function pizzaQue() {
+  alert("this is the start of PizzaQue");
+  var pizzaInfo = {};
+  var toppings = [];
+  var size = prompt('what size would you like it?')
+  pizzaInfo['size'] = size;
+  while(true){
+    var topping = prompt('what to you want on it?')
+    toppings.push(topping);
+    if(topping === ''){
+      pizzaInfo['toppings'] = toppings;
+      var newPizza = new Pizza(pizzaInfo);
+      console.log(currentPizza);
+      //add newPizza to order.pizzas
+      break;
+    }
+  }
+  alert("push Pizza!");
+  var addPizza = prompt('add a pizza?');
+  if(addPizza === "yes") {
+    pizzaQue();
+  }else{
+    return;
+  }
+}
+
+function orderQue() {
+  alert("this is the start of orderQue");
+  pizzaQue();
+  var addOrder = prompt('add an order?');
+  if (addOrder === 'yes') {
+    orderQue();
+  }else{
+    return;
+  }
+}
+
+
+$(function() {
+  prompt("hello! What's your name?");
+  orderQue();
+
+});
