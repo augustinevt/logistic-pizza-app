9f535ff110bd2768ac5827a89f8aabd37f767a30
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-06-19 21:35:51 -0700

    fixed the topping bug

--- js/ui.js
+++ js/ui.js
@@ -1,35 +1,43 @@
-
+var pizzaInfo = {toppings: []};
 
 function domify() {
   $('.order').remove();
   currentCustomer.orders.forEach(function(order) {
     $('body').append('<div class="order" id="order'+ order.id + '"></div>');
     $('#order' + order.id).append('<h3>'+order.id+'</h3>');
+    $('#order' + order.id).append('<h2 class="size">hello</h2>');
     $('#order' + order.id).append('<button type="button" id="order-'+order.id+'"></button>');
 
     // order.pizzas = [1,2,3,4]
 
-    for (var i = 0; i < order.pizzas.length; i++) {
-    $('#order' + order.id).append('<div class="pizzaIcon"></div>')
-    }
+    // for (var i = 0; i < order.pizzas.length; i++) {
+    // $('#order' + order.id).append('<div class="pizzaIcon"></div>')
+    // }
 
-    // order.pizzas.forEach(function() {
-    //   $('#order' + order.id).find(".orderPizzas").append('<div class="pizzaIcon"></div>')
-    // });
+     order.pizzas.forEach(function(pizza) {
+       var toppings = '';
+      pizza.toppings.forEach(function(topping, i){
+           toppings += "<h6>" + topping + "</h6>";
+       });
+
+      $('#order' + order.id).append('<div class="iconContainer"><h2 class=".size">' + pizza.size + '</h2><div class="pizzaIcon"></div>'+toppings+'</div>');
+
+    });
 
     $('#order-' + order.id).click(function(){
-      var pizzaInfo = {toppings: []};
       $('#cloud').fadeIn();
       $('pizzaForm').fadeIn();
 
       $('#addTopping').click(function(){
         pizzaInfo.toppings.push($('input[type="text"]').val());
         $('input[type="text"]').val('');
+
       });
 
       $('#createPizza').click(function(){
-        pizzaInfo['size'] = $('input[type="radio"]:checked')
+        pizzaInfo['size'] = $('input[type="radio"]:checked').val();
         order.pizzas.push(new Pizza(pizzaInfo, order));
+        pizzaInfo = {toppings: []};
         $('#cloud').fadeOut();
         $('pizzaForm').fadeOut();
         $(this).unbind();
